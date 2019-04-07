fd6f8e3c306a0e864d27af67835012e6c97f5b3f
fenglei <supersun_shine@126.com>
2019-04-07 17:36:29 +0800

    first commit

---
+++ APINet/APINetCommand.h
@@ -0,0 +1,17 @@
+//
+//  APINetCommand.h
+//  NetworkDemo
+//
+//  Created by 冯磊 on 2019/4/7.
+//  Copyright © 2019 super. All rights reserved.
+//
+
+#import "APINetManager.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface APINetCommand : APINetManager
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ APINet/APINetCommand.m
@@ -0,0 +1,13 @@
+//
+//  APINetCommand.m
+//  NetworkDemo
+//
+//  Created by 冯磊 on 2019/4/7.
+//  Copyright © 2019 super. All rights reserved.
+//
+
+#import "APINetCommand.h"
+
+@implementation APINetCommand
+
+@end
